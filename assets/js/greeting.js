19cd6ab1fa7c50281ddb40a41e8790eaed0e6d31
Winnerkorea2020 <68554587+Winnerkorea2020@users.noreply.github.com>
2022-01-24 00:30:16 +0900

    update

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -4,6 +4,8 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+const todoItems = document.querySelector("#todo");
+const TODOACT_CLASSNAME = "toto__activity";
 
 function paintGreeting(username) {
   greeting.innerText = `Hello! ${username}`;
@@ -16,15 +18,18 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   let username = loginInput.value;
   paintGreeting(username);
+  todoItems.classList.add(TODOACT_CLASSNAME);
   localStorage.setItem(USERNAME_KEY, username);
 }
 
 if (savedUsername === null) {
   // Show the form
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  todoItems.classList.add(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreeting(savedUsername);
+  todoItems.classList.add(TODOACT_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
